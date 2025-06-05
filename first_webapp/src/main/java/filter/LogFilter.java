97ff611834abbe99ed9146366fddc2ff339f395a
dir-FY25fukudaseiya <seiya.fukuda.v755@dirbato.co.jp>
2025-06-05 09:17:04 +0900

    コミット

---
+++ first_webapp/src/main/java/filter/LogFilter.java
@@ -0,0 +1,35 @@
+package filter;
+import java.time.LocalDateTime; 
+import java.io.IOException;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpFilter;
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * Servlet Filter implementation class LogFilter
+ */
+@WebFilter("/*")
+public class LogFilter extends HttpFilter implements Filter { 
+public LogFilter() { 
+super(); 
+} 
+public void destroy() { 
+} 
+/** 
+* @see Filter#doFilter(ServletRequest, ServletResponse, FilterChain) 
+*/
+ public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) 
+throws IOException, ServletException { 
+chain.doFilter(request, response); 
+// アクセスされた場所と日時を記録する
+System.out.println(((HttpServletRequest) request).getRequestURI() + ":" + LocalDateTime.now()); 
+} 
+public void init(FilterConfig fConfig) throws ServletException { 
+} 
+}
